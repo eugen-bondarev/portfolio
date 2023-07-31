b0002950a609af10416acd559aea983c6eb5bb90
Eugen Bondarev <eug.bondarev@gmail.com>
2023-07-31 21:44:57 +0200

    Installing wp-cli

--- app/web/app/plugins/portfolio-control/index.php
+++ app/web/app/plugins/portfolio-control/index.php
@@ -8,26 +8,70 @@
  * Text Domain: portfolio-control
  */
 
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'v2', '/export-db', [ 
-		'methods' => WP_REST_Server::READABLE,
-		'permission_callback' => '__return_true',
-		'callback' => function () {
-			return shell_exec( 'php wp-cli.phar --allow-root db export test.sql' );
-		}
-	] );
-	register_rest_route( 'v2', '/import-db', [ 
-		'methods' => WP_REST_Server::READABLE,
-		'permission_callback' => '__return_true',
-		'callback' => function () {
-			return shell_exec( 'php wp-cli.phar --allow-root db import test.sql' );
-		}
-	] );
-	register_rest_route( 'v2', '/test', [ 
-		'methods' => WP_REST_Server::READABLE,
-		'permission_callback' => '__return_true',
-		'callback' => function () {
-			return shell_exec( 'ls' );
-		}
-	] );
-} );
+class PortfolioControl
+{
+	public function __construct()
+	{
+		add_action('rest_api_init', fn () => $this->restAPIInit());
+	}
+
+	private function restAPIInit()
+	{
+		register_rest_route('v2', '/export-db', [
+			'permission_callback' => '__return_true',
+			'methods' => WP_REST_Server::READABLE,
+			'callback' => fn (...$args) => $this->exportDBCallback(...$args)
+		]);
+		register_rest_route('v2', '/import-db', [
+			'permission_callback' => '__return_true',
+			'methods' => WP_REST_Server::READABLE,
+			'callback' => fn (...$args) => $this->importDBCallback(...$args)
+		]);
+		register_rest_route('v2', '/test', [
+			'permission_callback' => '__return_true',
+			'methods' => WP_REST_Server::READABLE,
+			'callback' => fn (...$args) => $this->test(...$args)
+		]);
+	}
+
+	private function exportDBCallback(WP_REST_Request $request)
+	{
+		return 'exporting db..';
+	}
+
+	private function importDBCallback(WP_REST_Request $request)
+	{
+		return 'importing db..';
+	}
+
+	private function test(WP_REST_Request $request)
+	{
+		return shell_exec('ls /');
+	}
+}
+
+new PortfolioControl;
+
+// add_action('rest_api_init', function () {
+// 	register_rest_route('v2', '/export-db', [
+// 		'methods' => WP_REST_Server::READABLE,
+// 		'permission_callback' => '__return_true',
+// 		'callback' => function () {
+// 			return shell_exec('php wp-cli.phar --allow-root db export test.sql');
+// 		}
+// 	]);
+// 	register_rest_route('v2', '/import-db', [
+// 		'methods' => WP_REST_Server::READABLE,
+// 		'permission_callback' => '__return_true',
+// 		'callback' => function () {
+// 			return shell_exec('php wp-cli.phar --allow-root db import test.sql');
+// 		}
+// 	]);
+// 	register_rest_route('v2', '/test', [
+// 		'methods' => WP_REST_Server::READABLE,
+// 		'permission_callback' => '__return_true',
+// 		'callback' => function () {
+// 			return shell_exec('ls /var/www/html');
+// 		}
+// 	]);
+// });
