6dccdcba268e8ff41391469e3357a5aebdfac15e
Eugen Bondarev <eug.bondarev@gmail.com>
2024-06-23 17:04:20 +0200

    wip

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,12 +14,14 @@ export const ui = {
   en: {
     'nav.home': 'Home',
     'nav.about': 'About me',
+    'nav.tech-stack': 'Tech stack',
     'hero.title': 'Hi, I\'m Eugen',
     'hero.subtitle': 'I can build anything - scalable infrastructures, web apps, games, AI, astonishing UX/UI'
   },
   de: {
     'nav.home': 'Home',
     'nav.about': 'Über mich',
+    'nav.tech-stack': 'Tech-Stack',
     'hero.title': 'Hi, ich bin Eugen',
     'hero.subtitle': 'Ich kann alles - skalierbare Infrastrukturen, Web-Apps, Spiele, KI, atemberaubende UX/UI'
   },
