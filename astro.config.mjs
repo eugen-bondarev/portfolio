f220cc60610707b3cf4e01eb144c0265ac14e377
Eugen Bondarev <eug.bondarev@gmail.com>
2024-06-23 21:48:27 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap()],
