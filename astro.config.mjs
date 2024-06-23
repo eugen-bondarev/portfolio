6e74fe4e7548358d58b37b5ad652ec888d125cad
Eugen Bondarev <eug.bondarev@gmail.com>
2024-06-23 21:22:11 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
+  adapter: vercel(),
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap()],
 });
