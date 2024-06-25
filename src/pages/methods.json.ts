5d080964729d2b71cf5079df7214f2a892e99e85
Eugen Bondarev <eug.bondarev@gmail.com>
2024-06-25 16:09:12 +0200

    wip

--- src/pages/methods.json.ts
+++ src/pages/methods.json.ts
@@ -1,45 +1,36 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ params, request }) => {
-	try {
-		const flowluApiKey = import.meta.env.FLOWLU_API_KEY
+const foo = async ({ params, request }: any) => {
+	const flowluApiKey = import.meta.env.FLOWLU_API_KEY
 
-		const url = new URL(request.url);
+	const url = new URL(request.url);
 
-		const details = {
-			name: url.searchParams.get('name'),
-			description: url.searchParams.get('message'),
-		}
-
-		const formBody: string[] = [];
-		for (var property in details) {
-			var encodedKey = encodeURIComponent(property);
-			var encodedValue = encodeURIComponent(details[property]);
-			formBody.push(encodedKey + "=" + encodedValue);
-		}
-		const body = formBody.join("&");
-
-		const r = await fetch(`https://eugen-bondarev.flowlu.com/api/v1/module/crm/lead/create?api_key=${flowluApiKey}`, {
-			method: 'POST',
-			body,
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}
-		});
-
-		const json = await r.json()
-
-		if (!json.response?.id) {
-			throw new Error('an error occurred, ' + JSON.stringify(json))
-		}
-	} catch (e) {
-		return new Response(JSON.stringify({ msg: e.toString() }), {
-			status: 500, headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-
+	const details = {
+		name: url.searchParams.get('name'),
+		description: url.searchParams.get('message'),
 	}
+
+	const formBody: string[] = [];
+	for (var property in details) {
+		var encodedKey = encodeURIComponent(property);
+		var encodedValue = encodeURIComponent(details[property]);
+		formBody.push(encodedKey + "=" + encodedValue);
+	}
+	const body = formBody.join("&");
+
+	const r = await fetch(`https://eugen-bondarev.flowlu.com/api/v1/module/crm/lead/create?api_key=${flowluApiKey}`, {
+		method: 'POST',
+		body,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	});
+
+	const json = await r.json()
+}
+
+export const GET: APIRoute = ({ params, request }) => {
+	foo({ params, request })
 	return new Response(null, {
 		status: 200, headers: {
 			'Content-Type': 'application/json'
